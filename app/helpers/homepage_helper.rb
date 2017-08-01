ebda584c3b5f4ca817c380eff85d36b1d8d70451
Elton Okada <eltonokada@gmail.com>
2017-08-01 12:33:11 -0300

    Fixing image dimension

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -8,9 +8,18 @@ module HomepageHelper
   def with_first_listing_image(listing, &block)
     listing_model = Listing.find(listing.id)
     if listing_model.listing_images.size > 0
-      block.call(listing_model.listing_images.order("position").first.image.url)
+      block.call(listing_model.listing_images.order("position").first.image.url(:small_3x2))
     end
   end
+
+  # def with_first_listing_image(listing, &block)
+  #   Maybe(listing)
+  #     .listing_images
+  #     .map { |images| images.first }[:small_3x2].each { |url|
+  #     block.call(url)
+  #   }
+  # end
+
 
   def without_listing_image(listing, &block)
     if listing.listing_images.size == 0
