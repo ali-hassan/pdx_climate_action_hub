d466c9f37cb851c2bc1586b4a859730f845826f0
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2018-09-19 00:32:05 +0500

    Bug: Resolved stripe fields which were required to enable stripe payments

--- db/migrate/20180918172641_enable_stripe.rb
+++ db/migrate/20180918172641_enable_stripe.rb
@@ -1,7 +1,7 @@
 class EnableStripe < ActiveRecord::Migration[5.1]
   def up
     unless PaymentSettings.all.collect(&:payment_gateway).include?("stripe")
-      TransactionService::API::Api.settings.provision(community_id: 1, payment_gateway: :stripe, payment_process: :preauthorize, active: true)
+      TransactionService::API::Api.settings.provision(community_id: 1, payment_gateway: :stripe, payment_process: :preauthorize, active: true, api_verified: true, commission_from_seller: 0.5, minimum_price_cents: 50)
     end
   end
 
