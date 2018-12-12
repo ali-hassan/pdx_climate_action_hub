f9b0ca64cb0c3207bf810aa3ade8fcf7b98e470d
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2018-12-12 23:04:36 +0500

    Serach options with title and description of listings

--- app/controllers/admin/community_listings_controller.rb
+++ app/controllers/admin/community_listings_controller.rb
@@ -2,14 +2,19 @@ class Admin::CommunityListingsController < Admin::AdminBaseController
 
   def index
     @selected_left_navi_link = "listings"
-    @listings = resource_scope.order("#{sort_column} #{sort_direction}")
-      .paginate(:page => params[:page], :per_page => 30)
+    objects = params[:q].present? ? search_for_title : resource_scope
+    @listings = objects.order("#{sort_column} #{sort_direction}")
+                    .paginate(:page => params[:page], :per_page => 30)
   end
 
   private
 
   def resource_scope
     @current_community.listings.exist.includes(:author, :category)
+  end
+
+  def search_for_title
+    @current_community.listings.where("title LIKE (?) OR description LIKE (?)", "%#{params[:q]}%", "%#{params[:q]}%").includes(:author, :category)
   end
 
   def sort_column
