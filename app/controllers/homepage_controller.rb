e457397043b1fef3bf6d1b6e47e342014d242955
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-04-06 17:40:31 +0500

    Fixedd Issue

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -41,6 +41,21 @@ class HomepageController < ApplicationController
       @show_custom_fields = relevant_filters.present? || show_price_filter
       @category_menu_enabled = @show_categories || @show_custom_fields
     end
+    @selected_left_navi_link = "tribe_details"
+    # @community_customization is fetched in application_controller
+    @community_customizations ||= find_or_initialize_customizations(@current_community.locales)
+    all_locales = MarketplaceService.all_locales.map { |l|
+      {locale_key: l[:locale_key], translated_name: t("admin.communities.available_languages.#{l[:locale_key]}")}
+    }.sort_by { |l| l[:translated_name] }
+    enabled_locale_keys = available_locales.map(&:second)
+
+    @show_transaction_agreement = TransactionService::API::Api.processes.get(community_id: @current_community.id)
+                                      .maybe
+                                      .map { |data| has_preauthorize_process?(data) }
+                                      .or_else(nil).tap { |p| raise ArgumentError.new("Cannot find transaction process: #{opts}") if p.nil? }
+
+    make_onboarding_popup
+    @locale_selection_locals = { locale_selection_locals: { all_locales: all_locales, enabled_locale_keys: enabled_locale_keys, unofficial_locales: unofficial_locales }}
 
     listing_shape_param = params[:transaction_type]
 
@@ -144,7 +159,19 @@ class HomepageController < ApplicationController
   end
   # rubocop:enable AbcSize
   # rubocop:enable MethodLength
+  def update_user_language
+    process_locales = unofficial_locales.blank?
 
+    if process_locales
+      enabled_locales = params[:enabled_locales]
+      all_locales = MarketplaceService.all_locales.map{|l| l[:locale_key]}.to_set
+      enabled_locales_valid = enabled_locales.present? && enabled_locales.map{ |locale| all_locales.include? locale }.all?
+      if enabled_locales_valid
+        MarketplaceService.set_locales(@current_community, enabled_locales)
+        redirect_to("/", notice: "Successfully updated")
+      end
+    end
+  end
   private
 
   def parse_relevant_search_fields(params, relevant_filters)
@@ -369,4 +396,36 @@ class HomepageController < ApplicationController
     params.to_unsafe_hash
   end
 
+  def find_or_initialize_customizations(locales)
+    locales.inject({}) do |customizations, locale|
+      customizations[locale] = find_or_initialize_customizations_for_locale(locale)
+      customizations
+    end
+  end
+
+  def find_or_initialize_customizations_for_locale(locale)
+    @current_community.community_customizations.find_by_locale(locale) || build_customization_with_defaults(locale)
+  end
+
+  def build_customization_with_defaults(locale)
+    @current_community.community_customizations.build(
+        slogan: @current_community.slogan,
+        description: @current_community.description,
+        search_placeholder: t("homepage.index.what_do_you_need", locale: locale),
+        locale: locale
+    )
+  end
+
+  def unofficial_locales
+    all_locales = MarketplaceService.all_locales.map{|l| l[:locale_key]}
+    @current_community.locales.select { |locale| !all_locales.include?(locale) }
+        .map { |unsupported_locale_key|
+          unsupported_locale_name = Sharetribe::AVAILABLE_LOCALES.select { |l| l[:ident] == unsupported_locale_key }.map { |l| l[:name] }.first
+          {key: unsupported_locale_key, name: unsupported_locale_name}
+        }
+  end
+
+  def has_preauthorize_process?(processes)
+    processes.any? { |p| p.process == :preauthorize }
+  end
 end
